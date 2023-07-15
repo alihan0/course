821994249d0583f514c964bd6137154b1bb1cf3a
Alihan Öztürk <alihan@metatige.com>
2023-07-15 22:46:33 +0300

    login blade hazırlandı

--- resources/views/layout/auth/login.blade.php
+++ resources/views/layout/auth/login.blade.php
@@ -1,1 +1,35 @@
-login
+@extends('auth')
+
+@section('title', "Oturum Aç")
+
+@section('content')
+    
+<div class="app-auth-background">
+
+</div>
+<div class="app-auth-container">
+    <div class="logo">
+        <a href="index.html">Neptune</a>
+    </div>
+    <p class="auth-description">Please sign-in to your account and continue to the dashboard.<br>Don't have an account? <a href="sign-up.html">Sign Up</a></p>
+
+    <div class="auth-credentials m-b-xxl">
+        <label for="signInEmail" class="form-label">Email address</label>
+        <input type="email" class="form-control m-b-md" id="signInEmail" aria-describedby="signInEmail" placeholder="example@neptune.com">
+
+        <label for="signInPassword" class="form-label">Password</label>
+        <input type="password" class="form-control" id="signInPassword" aria-describedby="signInPassword" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
+    </div>
+
+    <div class="auth-submit">
+        <a href="#" class="btn btn-primary">Sign In</a>
+        <a href="#" class="auth-forgot-password float-end">Forgot password?</a>
+    </div>
+    <div class="divider"></div>
+    <div class="auth-alts">
+        <a href="#" class="auth-alts-google"></a>
+        <a href="#" class="auth-alts-facebook"></a>
+        <a href="#" class="auth-alts-twitter"></a>
+    </div>
+</div>
+@endsection
